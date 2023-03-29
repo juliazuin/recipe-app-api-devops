10b045f08c194fb6ac4009919163d48f76c11414
Julia Zuin <zuinjulia@hotmail.com>
2023-03-29 20:06:34 -0300

    update main with new params

---
+++ deploy/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.55.0"
+    }
+  }
+  backend "s3" {
+    bucket = "recipe-app-api-devops-tfstate-juliazuin2"
+    key = "recipe-app.tfstate"
+    region = "sa-east-1"
+    encrypt = true
+    dynamodb_table = "recipe-app-api-devops-tf-state-lock"
+  }
+}
+
+provider "aws" {
+    region = "sa-east-1"
+}
+
