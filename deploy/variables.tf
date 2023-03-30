a250763d34a98c83549f0eb20cad0da8de16105e
Julia Zuin <zuinjulia@hotmail.com>
2023-03-29 21:58:13 -0300

    Add terraform tags

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -2,3 +2,11 @@ variable "prefix" {
   # raad = recipe-app-api-devops
   default = "raad"
 }
+
+variable "project" {
+  default = "recipe-app-api-devops"
+}
+
+variable "contact" {
+  default = "email@londonappdev.com"
+}
