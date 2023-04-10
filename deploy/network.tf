2f9925a39ffc615aecba79e2011c73f74498d5c2
Julia Zuin <zuinjulia@hotmail.com>
2023-04-10 20:55:00 -0300

    add private subnets

--- deploy/network.tf
+++ deploy/network.tf
@@ -19,11 +19,11 @@ resource "aws_internet_gateway" "main" {
     tomap({ "Name" : "${local.prefix}-gw-main" })
   )
 }
-####################################################
-# Public subnets - Inbound/Outbound internet acces #
-
-# A Zone #
-####################################################
+#####################################################
+# Public subnets - Inbound/Outbound internet access #
+#                                                   #
+# A Zone Public                                     #
+#####################################################
 
 resource "aws_subnet" "public_a" {
   vpc_id = aws_vpc.main.id
@@ -40,7 +40,6 @@ resource "aws_subnet" "public_a" {
 
 
 data "aws_route_table" "public_a" {
-  subnet_id = var.subnet_id
   vpc_id    = aws_vpc.main.id
 
   tags = merge(
@@ -55,7 +54,7 @@ resource "aws_route_table_association" "public_a" {
 }
 
 resource "aws_route" "public_internet_access_a" {
-  route_table_id         = aws_route_table.public_a.id
+  route_table_id         = data.aws_route_table.public_a.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
@@ -81,7 +80,7 @@ resource "aws_nat_gateway" "public_a" {
 }
 
 ####################################################
-# B Zone #
+# B Zone Public                                    #
 ####################################################
 
 resource "aws_subnet" "public_b" {
@@ -97,9 +96,7 @@ resource "aws_subnet" "public_b" {
   )
 }
 
-
 data "aws_route_table" "public_b" {
-  subnet_id = var.subnet_id
   vpc_id    = aws_vpc.main.id
 
   tags = merge(
@@ -114,7 +111,7 @@ resource "aws_route_table_association" "public_b" {
 }
 
 resource "aws_route" "public_internet_access_b" {
-  route_table_id         = aws_route_table.public_b.id
+  route_table_id         = data.aws_route_table.public_b.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
@@ -137,4 +134,78 @@ resource "aws_nat_gateway" "public_b" {
     local.common_tags,
     tomap({ "Name" : "${local.prefix}-public-b" })
   )
+}
+
+####################################################
+# Private subnet - outbound internet access only   #
+#                                                  #
+# A Zone Private                                   #
+####################################################
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" : "${local.prefix}-private-a" })
+  )
+}
+
+data "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" : "${local.prefix}-private-a" })
+  )
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = data.aws_route_table.private_a.id
+}
+
+resource "aws_route" "private_internet_access_a" {
+  route_table_id         = data.aws_route_table.private_a.id
+  nat_gateway_id         = aws_nat_gateway.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+####################################################
+# B Zone Private                                   #
+####################################################
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" : "${local.prefix}-private-b" })
+  )
+}
+
+data "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" : "${local.prefix}-private-b" })
+  )
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = data.aws_route_table.private_b.id
+}
+
+resource "aws_route" "private_internet_access_b" {
+  route_table_id         = data.aws_route_table.private_b.id
+  nat_gateway_id         = aws_nat_gateway.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
