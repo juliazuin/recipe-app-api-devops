aa619f2ab18bce0c10e6e6f57ebf9f4ae1b6ff2b
Julia Zuin <zuinjulia@hotmail.com>
2023-04-13 21:06:45 -0300

    fix lf config

--- deploy/database.tf
+++ deploy/database.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "rds" {
 
 resource "aws_db_instance" "main" {
   identifier              = "${local.prefix}-db"
-  db_name                    = "recipe"
+  db_name                 = "recipe"
   allocated_storage       = 20
   storage_type            = "gp2"
   engine                  = "postgres"
