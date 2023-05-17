2ab2ab6e824266ee0d5630ae0f2c0c54fcc91abc
Julia Zuin <zuinjulia@hotmail.com>
2023-05-17 18:29:09 -0300

    add new entrypoint command

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,5 +3,9 @@
 set -e
 python manage.py collectstatic --noinput
 python manage.py wait_for_db
-python manage.py migrate
+
+echo "${0}: running migrations."
+python manage.py makemigrations --merge
+python manage.py migrate --noinput
+
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
