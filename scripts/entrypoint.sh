fba5592ce946d0037db03649946a639127aae3c8
Julia Zuin <zuinjulia@hotmail.com>
2023-05-17 21:57:33 -0300

    remove makemigrations

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,6 @@ python manage.py collectstatic --noinput
 python manage.py wait_for_db
 
 echo "${0}: running migrations."
-python manage.py makemigrations --merge
 python manage.py migrate --noinput
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
