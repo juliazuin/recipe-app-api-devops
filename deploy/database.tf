963ba9bdf502ec1ae36fcbac82d76cb034957a93
Julia Zuin <zuinjulia@hotmail.com>
2023-06-12 23:25:28 -0300

    feat(add-buckets3_config): add bucket s3 terraform config, parse env vars to container definition

--- deploy/database.tf
+++ deploy/database.tf
@@ -17,10 +17,10 @@ resource "aws_security_group" "rds" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description     = "Allow access to the RDS database instance"
-    protocol        = "tcp"
-    from_port       = 5432
-    to_port         = 5432
+    description = "Allow access to the RDS database instance"
+    protocol    = "tcp"
+    from_port   = 5432
+    to_port     = 5432
     security_groups = [
       aws_security_group.bastion.id,
       aws_security_group.ecs_service.id
