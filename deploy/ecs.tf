af21304cd09339097db07bebff7c5d2f56984131
Julia Zuin <zuinjulia@hotmail.com>
2023-06-18 22:16:37 -0300

    feat(change-file-formatting): change file fomatting from CRLF to LF

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -33,19 +33,8 @@ resource "aws_iam_policy" "task_execution_role_policy" {
 }
 
 resource "aws_iam_role" "task_execution_role" {
-  name = "${local.prefix}-task-exec-role"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Action" : "sts:AssumeRole",
-        "Principal" : {
-          "Service" : "ecs-tasks.amazonaws.com"
-        },
-        "Effect" : "Allow"
-      }
-    ]
-  })
+  name               = "${local.prefix}-task-exec-role"
+  assume_role_policy = jsonencode("./templates/ec2/assume-role-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "task_execution_role" {
@@ -54,19 +43,8 @@ resource "aws_iam_role_policy_attachment" "task_execution_role" {
 }
 
 resource "aws_iam_role" "app_iam_role" {
-  name = "${local.prefix}-api-task"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Action" : "sts:AssumeRole",
-        "Principal" : {
-          "Service" : "ecs-tasks.amazonaws.com"
-        },
-        "Effect" : "Allow"
-      }
-    ]
-  })
+  name               = "${local.prefix}-api-task"
+  assume_role_policy = jsonencode("./templates/ecs/assume-role-policy.json")
 
   tags = local.common_tags
 }
@@ -81,18 +59,15 @@ data "template_file" "api_container_definitions" {
   template = file("./templates/ecs/container-definitions.json.tpl")
 
   vars = {
-    app_image                = var.ecr_image_api
-    proxy_image              = var.ecr_image_proxy
-    django_secret_key        = var.django_secret_key
-    db_host                  = aws_db_instance.main.address
-    db_name                  = aws_db_instance.main.db_name
-    db_user                  = aws_db_instance.main.username
-    db_pass                  = aws_db_instance.main.password
-    log_group_name           = aws_cloudwatch_log_group.ecs_task_logs.name
-    log_group_region         = data.aws_region.current.name
-    allowed_hosts            = aws_lb.api.dns_name
-    s3_storage_bucket_name   = aws_s3_bucket.app_public_files.bucket
-    s3_storage_bucket_region = data.aws_region.current.name
+    app_image         = var.ecr_image_api
+    proxy_image       = var.ecr_image_proxy
+    django_secret_key = var.django_secret_key
+    db_host           = aws_db_instance.main.db_name
+    db_user           = aws_db_instance.main.username
+    db_pass           = aws_db_instance.main.password
+    log_group_name    = aws_cloudwatch_log_group.ecs_task_logs.name
+    log_group_region  = data.aws_region.current.name
+    allowed_hosts     = "*"
   }
 }
 
@@ -136,36 +111,29 @@ resource "aws_security_group" "ecs_service" {
   }
 
   ingress {
-    from_port = 8000
-    to_port   = 8000
-    protocol  = "tcp"
-    security_groups = [
-      aws_security_group.lb.id
-    ]
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = local.common_tags
 }
 
 resource "aws_ecs_service" "api" {
-  name             = "${local.prefix}-api"
-  cluster          = aws_ecs_cluster.main.name
-  task_definition  = aws_ecs_task_definition.api.family
-  desired_count    = 1
-  launch_type      = "FARGATE"
+  name            = "${local.prefix}-api"
+  cluster         = aws_ecs_cluster.main.name
+  task_definition = aws_ecs_task_definition.api.family
+  desired_count   = 1
+  launch_type     = "FARGATE"
   platform_version = "1.4.0"
 
   network_configuration {
     subnets = [
-      aws_subnet.private_a.id,
-      aws_subnet.private_b.id
+      aws_subnet.public_a.id,
+      aws_subnet.public_b.id
     ]
-    security_groups = [aws_security_group.ecs_service.id]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.api.arn
-    container_name   = "proxy"
-    container_port   = 8000
+    security_groups  = [aws_security_group.ecs_service.id]
+    assign_public_ip = true
   }
 }
